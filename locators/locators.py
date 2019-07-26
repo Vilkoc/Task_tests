a30e188c07bbde3240c426d78e36cfab90af0e03
Dmytro Babyuk <d_babyuk@yahoo.com>
2019-07-26 18:30:22 +0300

    Fixed1l

--- locators/locators.py
+++ locators/locators.py
@@ -2,7 +2,7 @@ class Locators():
     icon_css_selector = "img[class='rounded-circle img-responsive z-depth-0']"
     dropdown_css_selector = "a[class='dropdown-item']"
     login_btn_css_selector = "a[href='/users/auth']"
-    email_sign_in_id = "username"
+    email_sign_in_id = "username1"
     password_link_id = "password"
     sign_in_css_selector = "input[value='Sign In']"
 
