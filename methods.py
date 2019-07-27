194b9292fe87a7ee6c53117b1ec7c4776739ac79
Dmytro Babyuk <d_babyuk@yahoo.com>
2019-07-28 02:18:48 +0300

    wait_for added

--- methods.py
+++ methods.py
@@ -2,32 +2,58 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+types_By = {
+    'id': By.ID,
+    'css': By.CSS_SELECTOR,
+    'class': By.CLASS_NAME
+    }
+
 
 class Methods(object):
 
     def __init__(self, driver):
         self.driver = driver
+        self.types_By = types_By
 
-    def get_element(self, locator_type, locator, numb_list=0):
-        # This function seeks for elements
-        types_By = {
-            'id': By.ID,
-            'css': By.CSS_SELECTOR}
-        element = self.driver.find_elements(types_By[locator_type], locator)
-        return element[numb_list]
+    def get_element(self, locator_type, locator, numb_list=0, wait_id='PRESENCE'):
+        """This function seeks for all elements for specific locator and returns only numb_list-th element"""
+        try:
+            self.wait_for(locator_type, locator, wait_id)
+            element = self.driver.find_elements(self.types_By[locator_type], locator)
+            return element[numb_list]
+        except:
+            print("Exception on wait intercepted")
+            #self.get_element(locator_type, locator, numb_list=0, wait_id='PRESENCE')
 
-    def click_element(self, locator_type, locator, numb_list=0):
-        element = self.get_element(locator_type, locator, numb_list)
+
+    def click_element(self, locator_type, locator, numb_list=0, wait_id="PRESENCE"):
+        element = self.get_element(locator_type, locator, numb_list, wait_id)
         element.click()
 
-
-    def clear_element(self, locator_type, locator):
-        element = self.get_element(locator_type, locator)
+    def clear_element(self, locator_type, locator, numb_list=0, wait_id='PRESENCE'):
+        element = self.get_element(locator_type, locator, numb_list, wait_id)
         element.clear()
 
-    def send_keys(self, locator_type, locator, keys):
-        element = self.get_element(locator_type, locator)
+    def send_keys(self, locator_type, locator, keys, numb_list=0, wait_id='PRESENCE'):
+        element = self.get_element(locator_type, locator, numb_list, wait_id)
         element.send_keys(keys)
+
+    def wait_for(self, locator_type, locator, wait_id='PRESENCE'):
+        wait = WebDriverWait(self.driver, 10)
+        if wait_id == 'PRESENCE':
+            wait.until(EC.presence_of_all_elements_located((self.types_By[locator_type], locator)))
+        elif wait_id == 'VISIBLE':
+            wait.until(EC.visibility_of_element_located((self.types_By[locator_type], locator)))
+        elif wait_id == 'CLICKABLE':
+            wait.until(EC.element_to_be_clickable((self.types_By[locator_type], locator)))
+        else:
+            print('Incorrect WAITER, the default is used')
+            wait.until(EC.presence_of_all_elements_located((self.types_By[locator_type], locator)))
+
+
+
+
+
 
 
             
--- test1.py
+++ test1.py
@@ -4,11 +4,9 @@ import unittest
 from pages.home_page import HomePage
 from pages.sign_in_page import SignInPage
 from credentials import Credentials
-import time
 
 
 class Test1(BasePage):
-
 
     def test_login_logout(self, person='USER'):
         driver = self.driver
@@ -16,19 +14,14 @@ class Test1(BasePage):
         start = HomePage(driver)
         login = SignInPage(driver)
         start.click_icon()
-        time.sleep(1)
         start.click_login()
-        time.sleep(1)
 
         login.clear_box('email')
         login.send_keys('email', Credentials[person][0])
         login.clear_box('password')
         login.send_keys('password', Credentials[person][1])
         login.click_sign_in()
-        time.sleep(2)
         login.click_logout()
-        time.sleep(4)
-
 
 
 if __name__ == "__main__":
