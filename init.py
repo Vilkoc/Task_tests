1b27aca4e2b3bc9ee71985e7e5b701367015380e
Koreiba Roman <koreibaroman@gmail.com>
2019-08-02 16:24:01 +0300

    3nd try

--- init.py
+++ init.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
 from methods import Methods
 from pages.header import Header
+from driver_selection import WebdriverSelection
 from utilities.db import prepare_db
-from config import TIMEOUT
+from config import URL, TIMEOUT, WEBDRIVER
 
 
 class BasePage():
 
     @classmethod
     def setUpClass(cls):
-#        prepare_db()
-        cls.driver = webdriver.Chrome()
+        #        prepare_db()
+        cls.driver = WebdriverSelection().get_webdriver(WEBDRIVER)
         cls.driver.maximize_window()
-        cls.driver.get('http://localhost:4200')
+        cls.driver.get(URL)
         cls.browser = Methods(cls.driver, TIMEOUT)
         cls.header = Header(cls.driver)
 
--- methods.py
+++ methods.py
@@ -28,7 +28,7 @@ class Methods(object):
                 element.click()
 
     def clear_element(self, locator, text_value='default'):
-        """Clears the element with number elem_number"""
+        """Clears the element with the specific text_value"""
         WebDriverWait(self.driver, self.default_timeout).until(EC.presence_of_element_located(locator))
         elements = self.get_elements(locator)
         if text_value == 'default':
@@ -38,9 +38,8 @@ class Methods(object):
                 if element.text == text_value:
                     element.clear()
 
-
     def send_keys(self, locator, keys, text_value='default'):
-        """Send keys to the element with number elem_number"""
+        """Send keys to the element with the the specific text_value"""
         WebDriverWait(self.driver, self.default_timeout).until(EC.presence_of_element_located(locator))
         elements = self.get_elements(locator)
         if text_value == 'default':
@@ -50,12 +49,8 @@ class Methods(object):
                 if element.text == text_value:
                     element.send_keys(keys)
 
-
-    # def get_attr_value(self, locator, keys, attr):
-    #     """Send keys to the element with number elem_number"""
-    #     WebDriverWait(self.driver, self.default_timeout).until(EC.presence_of_element_located(locator))
-    #     elements = self.get_elements(locator)
-    #     for element in elements:
-    #         if element.text == text_value:
-    #             element.send_keys(keys)
-
+    def get_attr_value(self, locator, attr):
+        """Get attribute value of the element"""
+        WebDriverWait(self.driver, self.default_timeout).until(EC.visibility_of_element_located(locator))
+        element = self.get_elements(locator)[0]
+        return element.get_attribute(attr)
