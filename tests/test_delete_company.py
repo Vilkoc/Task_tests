a00600ae9c8ff0a224c40f6d69f06c85336ad685
Vilkoc <vilkoc@gmail.com>
2019-08-05 20:38:05 +0300

    add test data

---
+++ tests/test_delete_company.py
@@ -0,0 +1,20 @@
+from init import BasePage
+from pages.sign_in_page import SignInPage
+from pages.my_companies_page import MyCompaniesPage
+from data_tests.cowner_data import CownerData
+
+
+class TestDeleteCompany(BasePage):
+
+    def test_create_vacancy(self):
+        sign_in = SignInPage(self)
+        delete_company = MyCompaniesPage(self)
+
+        self.header.select_option(CownerData.OPTION)
+        sign_in.login(CownerData.PERSON)
+
+        self.header.click_my_companies()
+        delete_company.click_company_delete(CownerData.COMPANY_DELETE)
+
+        check = delete_company.check_company_present(CownerData.COMPANY_DELETE)
+        assert check
