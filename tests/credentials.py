5bf7c424b23523255a2ef279ff39befca3b1870d
Stanislav Diedyk <vilkoc@gmail.com>
2019-08-06 19:17:54 +0300

    test2

---
+++ tests/credentials.py
@@ -0,0 +1,5 @@
+Credentials = {
+    'USER': ('user@gmail.com', 'user'),
+    'COWNER': ('cowner@gmail.com', 'cowner'),
+    'ADMIN': ('admin@gmail.com', 'admin')
+}
---
+++ tests/init.py
@@ -0,0 +1,22 @@
+from methods import DriverWrapper
+from driver_selection import WebdriverSelection
+from config import URL, TIMEOUT, WEBDRIVER
+from pages.header import Header
+from utilities.db import prepare_db
+
+
+class BasePage:
+    """The parent class for all tests"""
+    @classmethod
+    def setUpClass(cls):
+        prepare_db()
+        driver = WebdriverSelection().get_webdriver(WEBDRIVER)
+        driver.maximize_window()
+        driver.get(URL)
+        cls.browser = DriverWrapper(driver, TIMEOUT)
+        cls.header = Header(cls)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.driver.quit()
